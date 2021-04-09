7eddfe4d7d0b2917d132ccaad1746e4cb6477b65
lenakazaryan <linakazaryan2019@gmail.com>
2021-04-09 22:38:21 +0400

    change PI constant part

--- Babel/math.js
+++ Babel/math.js
@@ -1,22 +1,22 @@
 export const Pow = function (base, exponent) {
-    let res = base;
-    for (let i = 1; i < exponent; i++) {
-      res *= base;
-    }
-    return res;
+  let res = base;
+  for (let i = 1; i < exponent; i++) {
+    res *= base;
+  }
+  return res;
+};
+
+export const hypotenuse = function (adjacent, opposite) {
+  let res = Math.sqrt(adjacent + opposite);
+  return {
+    adjacent,
+    opposite,
+    res,
   };
-  
-  export const hypotenuse = function (adjacent, opposite) {
-    let res = Math.sqrt(adjacent + opposite);
-    return {
-      adjacent,
-      opposite,
-      res,
-    };
-  };
-  
-  export const circleArea = function (radius) {
-    return 2 * Math.PI * radius;
-  };
-  
-  export const PI = Math.PI;
+};
+
+export const circleArea = function (radius) {
+  return 2 * PI * radius;
+};
+
+export const PI = Math.PI;
